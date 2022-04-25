4c1e08dcccf0d14751cd42602463f07713e14f75
User <deduhin1234@gmail.com>
2022-04-25 11:12:52 +0500

    hello

--- игра.py
+++ игра.py
@@ -17,3 +17,39 @@ class TCar:
     def move(self):
         self.x+=self.v
         if self.x>self.road.lendth:
+            self.x=0
+class TLight:
+    red=0
+    redyellow=1
+    green=2
+    yellow=3
+    def __init__(self, x_user,road_user):
+        self.road=road_user
+        if self.road.lendth>x_user and x_user>=0:
+            self.x=x_user
+        else:
+            self.x=0
+        self.stage=self.red
+    def timer(self):
+        pass
+
+n=3
+road=TRoad(120,n)
+
+cars=[]
+for i in range(n):
+    cars.append(TCar(road,i+1,(i+1)*2 ))
+print(f'Дорога: длинна={road.lendth}, ширина={road.width} полос')
+print('Свойства машин:{}')
+for i in range(n):
+    print(f'{i}: Полоса ={cars[i].p}, скорость ={cars[i].v} x={cars[i].x}')
+time=10
+for i in range(time):
+    for car in cars:
+        car.move()
+print('Свойства машин:{}')
+for i in range(n):
+    print(f'{i}: Полоса ={cars[i].p}, скорость ={cars[i].v} x={cars[i].x}')
+
+light=TLight(road, 50)
+print(light.x)
